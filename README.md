4d7adf6dd798cd8f5b266bebeffc023eba43a888
AtishJoottun <80235051+AtishJoottun@users.noreply.github.com>
2023-12-19 14:53:01 +0400

    Modified to add more website to test and change the OutText.txt to show analytics

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 
 [source of the tldr_fail_test.py](https://gist.github.com/dadrian/f51e7f96aa659937775232cc3576e5f8#file-tldr_fail_test-py)
 
-This script check for the tldr fail bug on the most popular website in mauritius:
+This script check for the tldr fail bug on the most popular website in Mauritius:
 [Most popular website](https://webrate.org/countries/statistics/MU)
 
-The OutText.txt show my output on the websites.
+The OutText.txt shows:
+1. The list of websites
+2. list of websites that failed
+3. the percentage of wesite that failed.
--- Script.py
+++ Script.py
@@ -1,11 +1,34 @@
+Error_Count = 0
+
 import os
+import sys
 
 list = ["google.mu", "myt.mu", "mcb.mu", "okta.com", "topfmradio.com", "freshdesk.com", "govmu.org", "lexpress.mu",
-        "inside.news", "intnet.mu", "priceguru.mu", "myjob.mu", "mauritiustelecom.com", "gceguide.com", "vbazz.com"]
+        "inside.news", "intnet.mu", "priceguru.mu", "myjob.mu", "mauritiustelecom.com", "gceguide.com", "vbazz.com",
+        "mauritiusturfclub.com", "uom.ac.mu", "lemauricien.com", "france24.com", "newsnow.co.uk", "ib.mcb.mu",
+        "moodfeed.net", "pixhost.to", "journee-mondiale.com", "frappe.cloud", "partsouq.com", "riddimsworld.com",
+        "fnb.co.za", "naukrigulf.com", "airmauritius.com", "spikbuy.network", "sfimg.com", "mra.mu", "mega.mu",
+        "instantly.ai", "ifvod.tv", "refinitiv.com", "mycar.mu", "canalplus.com", "devskiller.com", "Ebmu.sbmgroup.mu",
+        "Cyberstorm.mu"]
+
+error_list = []
+Site_count = len(list)
+print(f"Number of Website to check: {Site_count}")
 
 
 for item in list:
     command = "python3 tldr_fail_test.py " + item
     print(command)
-    os.system(command=command)
-    print("---------------------------------------------------")
+    if os.system(command=command) != 0:
+        Error_Count +=1
+        error_list.append(item)
+
+
+file = open("OutText.txt", "w")
+file.write(f"Total number of website: {Site_count}\n")
+file.write(f"List of website: {list}\n")
+file.write(f"Number of errors: {Error_Count}\n")
+file.write(f"List of website with errors: {error_list}\n")
+file.write(f"Percentage of error: {(Error_Count/Site_count)*100}\n")
+
+file.close()
