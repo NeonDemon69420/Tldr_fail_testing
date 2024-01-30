47cef1d76f7d06dbd40e74c6a804f3e9483fbb0f
poshan-p <poshanpeeroo@gmail.com>
2024-01-30 22:23:36 +0400

    Added IP vulnerability test and netname classification

--- utils/multiprocessing.py
+++ utils/multiprocessing.py
@@ -7,12 +7,45 @@ def worker_get_valid_ip_addresses(process_num, manager, number_of_processes, ret
 
     for ip_address in list:
         print(f"Checking ip address {ip_address} if port 443 is open.")
-
         if get_valid_ip_addresses(ip_address):
             append_to_array(manager, str(process_num), ip_address, return_dictionary)
 
-# number_of_processes must be a power of 2 (i.e., 2, 4, 8, 16, ...)
-def start_multiprocessing(number_of_processes, worker, *args):
+
+def worker_check_ip_addresses_tldr(process_num, manager, number_of_processes, return_dictionary, *args):
+    list_length = int(len(args[0])/number_of_processes)
+    list = args[0][list_length*process_num:list_length*(process_num+1)]
+
+    for i, ip_address in enumerate(list):
+        print(f"Process {process_num} is {(i/len(list))*100:.2f}% done.")
+        try:
+            result = subprocess.run(["python3", "./tldr_fail_test.py", ip_address], stdout=subprocess.PIPE, text=True, timeout=10)
+
+            if not "[WinError 10054]" in result.stdout:
+                append_to_array(manager, str(process_num), (ip_address, False), return_dictionary)
+            else:
+                append_to_array(manager, str(process_num), (ip_address, True), return_dictionary)
+        except subprocess.TimeoutExpired:
+            append_to_array(manager, str(process_num), (ip_address, True), return_dictionary)
+
+def worker_classify_ip_addresses(process_num, manager, number_of_processes, return_dictionary, *args):
+    list_length = int(len(args[0])/number_of_processes)
+    list = args[0][list_length*process_num:list_length*(process_num+1)]
+    
+    for i, ip_address in enumerate(list):
+        print(f"Process {process_num} is {(i/len(list))*100:.2f}% done.")
+        try:
+            result = subprocess.run(["nmap", ip_address.strip(), "--script", "whois-ip.nse", "-Pn"], stdout=subprocess.PIPE, text=True)
+            netname = extract_netname(result.stdout)
+            if netname == "":
+                append_to_array(manager, "Unknown", ip_address, return_dictionary)
+            else:
+                append_to_array(manager, netname, ip_address, return_dictionary)
+        except subprocess.TimeoutExpired:
+            append_to_array(manager, "Unknown", ip_address, return_dictionary)
+
+
+
+def start_multiprocessing(number_of_processes, worker, multiprocessing_return_function, *args):
     manager = multiprocessing.Manager()
     return_dictionary = manager.dict()
     jobs = []
@@ -25,8 +58,13 @@ def start_multiprocessing(number_of_processes, worker, *args):
     for proc in jobs:
         proc.join()
 
-    flattened_list = [item for sublist in return_dictionary.values() for item in sublist]
-    return flattened_list
+    return multiprocessing_return_function(return_dictionary)
+
+def default_multiprocessing_return_function(return_dictionary):
+    return [item for sublist in return_dictionary.values() for item in sublist]
+
+def classify_ip_addresses_return_function(return_dictionary):
+    return return_dictionary
 
 
 def append_to_array(manager, key, value, dict):
@@ -54,3 +92,10 @@ def get_valid_ip_addresses(ip):
     else:
         print(f"Port 80 and 443 is not open for ip address {ip}")
         return False
+    
+def extract_netname(result):
+    netname_index = result.find('netname')
+    netname_end_index = result.find('\n', netname_index)
+    netname_line = result[netname_index:netname_end_index]
+    netname_parts = netname_line.split(':')
+    return netname_parts[-1].strip()
